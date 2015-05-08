2c7a29a80170bfcf43f0faf5e3bb7673888f579f
panicbit <panicbit@users.noreply.github.com>
2015-05-08 20:52:42 +0200

    Add remaining methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 //! See [augeas.h](https://github.com/hercules-team/augeas/blob/release-1.3.0/src/augeas.h) for details
 extern crate libc;
 
-use self::libc::{c_void, c_char, c_int, c_uint};
+use self::libc::{c_void, c_char, c_int, c_uint, FILE};
+
 
 #[repr(C)]
 #[derive(Copy,Clone)]
@@ -26,17 +27,48 @@ impl std::ops::BitOr for AugFlag {
     }
 } 
 
+/// Opaque augeas type
 #[allow(non_camel_case_types)]
 pub type augeas_t = *mut c_void;
+/// Opaque xmlNode type
+#[allow(non_camel_case_types)]
+pub enum xmlNode {}
 
 #[link(name = "augeas")]
 extern {
     pub fn aug_init(root: *const c_char, loadpath: *const c_char, flags: c_uint) -> augeas_t;
     pub fn aug_defvar(aug: augeas_t, name: *const c_char, expr: *const c_char) -> c_int;
-    pub fn aug_get(aug: augeas_t, path: *const c_char, value: *mut *const c_char) -> c_int;
+    pub fn aug_defnode(aug: augeas_t, name: *const c_char, expr: *const c_char, value: *const c_char, created: *mut c_int) -> c_int;
+    pub fn aug_get(aug: augeas_t, path: *const c_char, value: *mut *mut c_char) -> c_int;
     pub fn aug_label(aug: augeas_t, path: *const c_char, label: *mut *const c_char) -> c_int;
-    pub fn aug_close(aug: augeas_t);
-    pub fn aug_save(aug: augeas_t) -> c_int;
     pub fn aug_set(aug: augeas_t, path: *const c_char, value: *const c_char) -> c_int;
+    pub fn aug_setm(aug: augeas_t, base: *const c_char, sub: *const c_char, value: *const c_char) -> c_int;
+    pub fn aug_span(aug: augeas_t, path: *const c_char, filename: *mut *mut c_char,
+        label_start: *mut c_uint, label_end: *mut c_uint,
+        value_start: *mut c_uint, value_end: *mut c_uint,
+        span_start: *mut c_uint, span_end: *mut c_uint
+    ) -> c_int;
+    pub fn aug_insert(aug: augeas_t, path: *const c_char, label: *const c_char, before: c_int) -> c_int;
+    pub fn aug_rm(aug: augeas_t, path: *const c_char) -> c_int;
+    pub fn aug_mv(aug: augeas_t, src: *const c_char, dst: *const c_char) -> c_int;
+    pub fn aug_cp(aug: augeas_t, src: *const c_char, dst: *const c_char) -> c_int;
+    pub fn aug_rename(aug: augeas_t, src: *const c_char, lbl: *const c_char) -> c_int;
     pub fn aug_match(aug: augeas_t, path: *const c_char, matches: *mut *mut *mut c_char ) -> c_int;
+    pub fn aug_save(aug: augeas_t) -> c_int;
+    pub fn aug_load(aug: augeas_t) -> c_int;
+    pub fn aug_text_store(aug: augeas_t, lens: *const c_char,
+        node: *const c_char, path: *const c_char) -> c_int;
+    pub fn aug_text_retrieve(aug: augeas_t, lens: *const c_char,
+        node_in: *const c_char, path: *const c_char,
+        node_out: *const c_char) -> c_int;
+    pub fn aug_print(out: *mut FILE, path: *const c_char) -> c_int;
+    pub fn aug_to_xml(aug: augeas_t, path: *const c_char, xmldoc: *mut *mut xmlNode,
+        flags: c_uint) -> c_int;
+    pub fn aug_transform(aug: augeas_t, lens: *const c_char, file: *const c_char, excl: c_int) -> c_int;
+    pub fn aug_srun(aug: augeas_t, out: *mut FILE, text: *const c_char) -> c_int;
+    pub fn aug_close(aug: augeas_t);
+    pub fn aug_error(aug: augeas_t) -> c_int;
+    pub fn aug_error_message(aug: augeas_t) -> *const c_char;
+    pub fn aug_error_minor_message(aug: augeas_t) -> *const c_char;
+    pub fn aug_error_details(aug: augeas_t) -> *const c_char;
 }
