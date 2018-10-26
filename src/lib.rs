6a6811e20470d99da3de1b99b8869bd5171576b1
David Lutterkort <lutter@watzmann.net>
2018-10-25 21:59:25 -0700

    Add binding for aug_load_file

--- src/lib.rs
+++ src/lib.rs
@@ -315,6 +315,12 @@ impl Augeas {
         self.make_result(s)
     }
 
+    pub fn load_file(&mut self, file: &str) -> Result<()> {
+        let file = CString::new(file)?;
+
+        unsafe { aug_load_file(self.ptr, file.as_ptr()) };
+        self.make_result(())
+    }
 }
 
 impl Augeas {
@@ -549,6 +555,20 @@ fn escape_test() {
 
     let n = aug.escape_name("foo[");
     assert_eq!(Ok(Some(String::from("foo\\["))), n);
+}
+
+#[test]
+fn load_file_test() {
+    let mut aug = Augeas::init("tests/test_root", "", Flags::NoLoad).unwrap();
+
+    aug.load_file("/etc/passwd").unwrap();
+    let root = aug.get("etc/passwd/root/uid").unwrap();
+    assert!(root.is_some());
+
+    let err = aug.load_file("/var/no/lens/for/this");
+    assert!(err.is_err());
+    let e = err.err().unwrap();
+    assert!(e.is_code(ErrorCode::NoLens));
 }
 
 #[test]
